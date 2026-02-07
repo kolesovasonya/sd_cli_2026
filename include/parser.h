90c33435607d4961aba37d9cf87862b1e27c17b8
alina chernokulskaia <alina.chernok@gmail.com>
2026-02-08 01:09:19 +0300

    feat: implement parser

---
+++ include/parser.h
@@ -0,0 +1,37 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "lexer.h"
+#include <memory>
+#include <vector>
+
+class AbstractCommand;
+class EnvironmentManager;
+
+/**
+ * @brief Parses tokens into executable command objects
+ */
+class Parser {
+public:
+    /**
+     * @brief Constructs parser with environment manager
+     * @param envManager Environment manager for variable resolution
+     */
+    explicit Parser(EnvironmentManager& envManager);
+    
+    /**
+     * @brief Parses tokens into command object
+     * @param tokens Vector of tokens to parse
+     * @return Unique pointer to command, or nullptr if no command
+     */
+    std::unique_ptr<AbstractCommand> parse(const std::vector<Token>& tokens);
+    
+private:
+    bool isAssignment(const std::vector<Token>& tokens);
+    void handleAssignment(const std::vector<Token>& tokens);
+    std::string resolveValue(const Token& token);
+    
+    EnvironmentManager& envManager_;
+};
+
+#endif
---
+++ src/parser.cpp
@@ -0,0 +1,56 @@
+#include "parser.h"
+#include "environment_manager.h"
+#include "command_factory.h"
+#include "commands/abstract_command.h"
+
+Parser::Parser(EnvironmentManager& envManager) : envManager_(envManager) {}
+
+std::unique_ptr<AbstractCommand> Parser::parse(const std::vector<Token>& tokens) {
+    if (tokens.empty()) {
+        return nullptr;
+    }
+    
+    if (isAssignment(tokens)) {
+        handleAssignment(tokens);
+        return nullptr;
+    }
+    
+    std::vector<std::string> args;
+    
+    for (const auto& token : tokens) {
+        args.push_back(resolveValue(token));
+    }
+    
+    if (args.empty()) {
+        return nullptr;
+    }
+    
+    std::string commandName = args[0];
+    args.erase(args.begin());
+    
+    CommandFactory factory;
+    return factory.createCommand(commandName, args);
+}
+
+bool Parser::isAssignment(const std::vector<Token>& tokens) {
+    return tokens.size() == 1 && tokens[0].type == TokenType::ASSIGNMENT;
+}
+
+void Parser::handleAssignment(const std::vector<Token>& tokens) {
+    std::string assignment = tokens[0].value;
+    size_t eqPos = assignment.find('=');
+    
+    if (eqPos != std::string::npos) {
+        std::string name = assignment.substr(0, eqPos);
+        std::string value = assignment.substr(eqPos + 1);
+        envManager_.setVariable(name, value);
+    }
+}
+
+std::string Parser::resolveValue(const Token& token) {
+    if (token.type == TokenType::WORD && !token.value.empty() && token.value[0] == '$') {
+        std::string varName = token.value.substr(1);
+        return envManager_.getVariable(varName);
+    }
+    return token.value;
+}
